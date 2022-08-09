abc672fd6bee49520aa1b30a55579218f079e9b4
Silas <azolyth@gmail.com>
2022-08-09 00:18:47 -0700

    Setup nav icons

---
+++ Dog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import data from './data.js';
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+// Remember to import the data and Dog class!
+import data from './data.js';
+
+console.log('index.js is running...');
