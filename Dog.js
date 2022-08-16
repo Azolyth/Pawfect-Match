9805559be7cb4828ce5f2c141d0ee3b4215d3719
Silas <azolyth@gmail.com>
2022-08-15 17:39:24 -0700

    Setup like/dislike handler

--- Dog.js
+++ Dog.js
@@ -6,6 +6,24 @@ class Dog {
   constructor(data) {
     Object.assign(this, data);
   }
+
+  setMatchStatus(bool) {
+    this.hasBeenLiked = bool;
+    this.hasBeenSwiped = true;
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio } = this;
+
+    return `
+    <div class="profile-bio">
+            <h3>${name}, ${age}</h3>
+            <p>${bio}</p>
+          </div>
+          <img src=${avatar} class="profile-picture"
+          />
+    `;
+  }
 }
 
 export default Dog;
--- index.js
+++ index.js
@@ -3,26 +3,46 @@
 import data from './data.js';
 import Dog from './Dog.js';
 
-let hasBeenLiked = false;
-let hasBeenDisliked = false;
+let currentIndex = 0;
+let currentDog = new Dog(data[currentIndex]);
+
+const likeButton = document.getElementById('like-button');
+const dislikeButton = document.getElementById('cross-button');
+
+// Renders getDogHtml onto DOM
+const render = () => {
+  document.getElementById('profile-picture-card').innerHTML = currentDog.getDogHtml();
+};
+// Calling render
+render();
+
+// Increases index and assigns current index for re-render
+const getNextDog = () => {
+  currentIndex++;
+  currentDog = new Dog(data[currentIndex]);
+  render();
+};
+
+const likeHandler = () => {
+  currentDog.setMatchStatus(true);
+  profileStatus();
+  setTimeout(getNextDog, 1000);
+};
+
+const dislikeHandler = () => {
+  currentDog.setMatchStatus(false);
+  profileStatus();
+  setTimeout(getNextDog, 1000);
+};
 
 const profileStatus = () => {
-  let likeStatus = hasBeenLiked ? 'like' : 'nope';
   let profileStatusHtml = '';
-
+  let likeStatus = currentDog.hasBeenLiked ? 'like' : 'nope';
   profileStatusHtml = `
     <img class="profile-rating" src="./images/badge-${likeStatus}.png">
     `;
-  return (document.getElementById('profile-rating').innerHTML =
-    profileStatusHtml);
+  return (document.getElementById('profile-rating').innerHTML = profileStatusHtml);
 };
 
-document.getElementById('like-button').addEventListener('click', () => {
-  hasBeenLiked = true;
-  profileStatus();
-});
-
-document.getElementById('cross-button').addEventListener('click', () => {
-  hasBeenLiked = false;
-  profileStatus();
-});
+likeButton.addEventListener('click', likeHandler);
+dislikeButton.addEventListener('click', dislikeHandler);
